3dfedac8eebaa1e295ab4938dbe1009998843979
Jeong-w-o-o <irmnels@gmail.com>
2022-02-13 03:38:36 +0900

    [FEAT] 프로그래머스 12926 해결

--- src/programmers/level1/sol12926.java
+++ src/programmers/level1/sol12926.java
@@ -8,17 +8,38 @@ package programmers.level1;
 public class sol12926 {
     public String solution(String s, int n) {
         String answer = "";
+        char[] ch = s.toCharArray();
 
-        for(int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
-            if(Character.isLowerCase(ch)) {
-                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+        for(int i = 0; i < ch.length; i++) {
+            if(ch[i] >= 'a' && ch[i] <= 'z') {
+                if(ch[i] + n > 'z') {
+                    ch[i] += n - 26;
+                }
+                else {
+                    ch[i] += n;
+                }
             }
-            else if(Character.isUpperCase(ch)) {
-                 ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            else if (ch[i] >= 'A' && ch[i] <= 'Z') {
+                if(ch[i] + n > 'Z') {
+                    ch[i] += n - 26;
+                }
+                else {
+                    ch[i] += n;
+                }
             }
-            answer += ch;
+            answer = String.valueOf(ch);
         }
         return answer;
+//        for(int i=0; i<s.length(); i++) {
+//            char ch = s.charAt(i);
+//            if(Character.isLowerCase(ch)) {
+//                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+//            }
+//            else if(Character.isUpperCase(ch)) {
+//                 ch = (char) ((ch - 'A' + n) % 26 + 'A');
+//            }
+//            answer += ch;
+//        }
+//        return answer;
     }
 }
